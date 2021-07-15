381b706f139d68edbba5c097f33a7fa78ef62165
Isaacjohnson <isaac.designws@gmail.com>
2021-07-15 10:07:45 -0500

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+
+export default class EyesOnMe extends React.Component {
+    
+focusFinder = () => {
+    console.log('Good!')
+}
+
+blurLogger = () => {
+    console.log('Hey! Eyes on me!')
+}
+    
+    render() {
+
+        return (
+            <button onFocus={this.focusFinder} onBlur={this.blurLogger}>
+
+            </button>
+        )
+    }
+}
